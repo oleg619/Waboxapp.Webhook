41f0ae29d5c2e4848484d76d146065ac12132a4d
Oleh Hrechukh <grechukh619@gmail.com>
2020-05-29 10:15:33 +0300

    Added get method

--- src/Waboxapp.Webhook/Controllers/WaboxappController.cs
+++ src/Waboxapp.Webhook/Controllers/WaboxappController.cs
@@ -1,18 +1,40 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace Waboxapp.Webhook.Controllers
 {
+    public class Storage
+    {
+        private readonly List<Update> _messages = new List<Update>();
+
+        public void Add(Update text)
+        {
+            _messages.Add(text);
+        }
+
+        public IEnumerable<Update> GetAll() => _messages;
+    }
+
     [Route("api/[controller]")]
     public class WaboxappController : Controller
     {
+        private readonly Storage _storage;
         private readonly ILogger<WaboxappController> _logger;
 
         public WaboxappController(
+            Storage storage,
             ILogger<WaboxappController> logger)
         {
+            _storage = storage;
             _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_storage.GetAll());
         }
 
         [HttpPost("update")]
@@ -20,7 +42,9 @@ namespace Waboxapp.Webhook.Controllers
         public Task Update([FromForm] Update update)
         {
             _logger.LogInformation("We received new {@message}", update);
-            
+
+            _storage.Add(update);
+
             return Task.CompletedTask;
         }
     }
@@ -48,7 +72,7 @@ namespace Waboxapp.Webhook.Controllers
         public string Event { get; set; }
         public string Token { get; set; }
         public long Uid { get; set; }
-        
+
         public Contact Contact { get; set; }
         public Message Message { get; set; }
     }
--- src/Waboxapp.Webhook/Startup.cs
+++ src/Waboxapp.Webhook/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Waboxapp.Webhook.Controllers;
 
 namespace Waboxapp.Webhook
 {
@@ -16,6 +17,7 @@ namespace Waboxapp.Webhook
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<Storage>();
             services.AddControllers();
         }
 
